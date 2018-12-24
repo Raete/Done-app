cbb9bedfb771ebaba583a607b587754a70044f9c
reate <kattovera00@gmail.com>
2018-12-24 20:43:22 +0100

    edit readme, public versin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyBxahBQwNfeS8ECEZ2vyPzs4BqHmos2MLs",
-        authDomain: "done-app-ang.firebaseapp.com",
-        databaseURL: "https://done-app-ang.firebaseio.com",
-        projectId: "done-app-ang",
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
         storageBucket: "",
-        messagingSenderId: "171312576212"
+        messagingSenderId: ""
     }
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyBxahBQwNfeS8ECEZ2vyPzs4BqHmos2MLs",
-        authDomain: "done-app-ang.firebaseapp.com",
-        databaseURL: "https://done-app-ang.firebaseio.com",
-        projectId: "done-app-ang",
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
         storageBucket: "",
-        messagingSenderId: "171312576212"
+        messagingSenderId: ""
     }
   };
   
